6bc400a89fd2f5b055400eabe5fe4c05f6239a67
AnaMCS26 <144897504+AnaMCS26@users.noreply.github.com>
2024-02-13 17:15:32 +0000

    igual no funciona

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,44 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext.js";
 import "../../styles/home.css";
+import People from "./people.jsx";
+import Planets from "./planets.jsx";
+import Starships from "./Starships.jsx";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  const { store } = useContext(Context);
+
+  const cardStyle = { width: '18rem' };
+  return (
+    <div className="container">
+      <>
+        <h2 className="text-left mt-3 mb-4">Personajes</h2>
+        <div className="d-flex flex-nowrap" style={{ overflowX: 'auto' }}>
+          {store.peopleInfo && store.peopleInfo.map((item) => 
+            <div key={item.url} style={cardStyle}>
+              <People uid={item.uid} name={item.name} />
+            </div>
+          )}
+        </div>
+
+        <h2 className="text-left mt-3 mb-4">Planetas</h2>
+        <div className="planets" style={{ width: '100%', overflowX: 'auto' }}>
+          <div className="planetCards">
+            <div className="planetLot">
+              {store.planetsInfo.map((item) => <Planets uid={item.uid} key={item.url} name={item.name} />)}
+            </div>
+          </div>
+        </div>
+
+        <h2 className="text-left mt-3 mb-4">Naves</h2>
+        <div className="starships" style={{ width: '100%', overflowX: 'auto' }}>
+          <div className="starshipCards">
+            <div className="starshipLot">
+              {store.starshipsInfo.map((item) => <Starships uid={item.uid} key={item.url} name={item.name} />)}
+            </div>
+          </div>
+        </div>
+      </>
+    </div>
+  );
+};
---
+++ src/js/views/specificPeople.jsx
@@ -0,0 +1,55 @@
+import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+const SpecificPeople = () => {
+    const { store, actions } = useContext(Context);
+    const [myPeople, setMyPeople] = useState({});
+    const params = useParams();
+
+    useEffect(() => {
+        fetch("https://www.swapi.tech/api/people/" + params.uid)
+            .then(response => response.json())
+            .then((data) => setMyPeople(data.result.properties));
+    }, []);
+
+    const imageURL = `https://starwars-visualguide.com/assets/img/characters/${params.uid}.jpg`;
+
+    return (
+        <div className="container my-4">
+            <div className="row justify-content-center">
+                <div className="col-md-3 pe-1">
+                    <div className="card">
+                        <img
+                            src={imageURL}
+                            onError={({ currentTarget }) => {
+                                currentTarget.onerror = null;
+                                currentTarget.src = "https://starwars-visualguide.com/assets/img/placeholder.jpg";
+                            }}
+                            className="card-img-top"
+                            alt="People"
+                        />
+                    </div>
+                </div>
+                <div className="col-md-7 ps-1">
+                    <div className="card">
+                        <div className="card-body">
+                            <h2 className="card-title text-dark">{myPeople.name}</h2>
+                            <ul className="list-unstyled">
+                                <li><strong>Género: </strong> {myPeople.gender}</li>
+                                <li><strong>Nacimiento: </strong>{myPeople.birth_year}</li>
+                                <li><strong>Color de Ojos: </strong>{myPeople.eye_color}</li>
+                                <li><strong>Películas en las que aparece: </strong>{myPeople.films}</li>
+                                <li><strong>Color de Cabello: </strong>{myPeople.hair_color}</li>
+                                <li><strong>Altura: </strong>{myPeople.height}</li>
+                                <li><strong>Hogar: </strong>{myPeople.homeworld}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SpecificPeople;
